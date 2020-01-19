e734d875f9c393b13e5a6ef8c2ca1f85d2945d37
Ritwick Biswas <ritwick.biswas95@gmail.com>
2020-01-19 08:40:56 +0530

    Node Basics

--- First/app.js
+++ First/app.js
@@ -1,1 +1,15 @@
+var time = 0 ;
 console.log("Hello");
+
+// Executes the fucntion after 3 seconds
+setTimeout(function(){
+    console.log("3 Sec Later..");
+},3000);
+
+var timer = setInterval(function(){ //this function runs for infinity. Make sure to stop it programatically.
+    time+=2;
+    console.log(time + " Seconds have passed");
+    if(time > 5){
+        clearInterval(timer); //clears the time
+    }
+},2000);
